1c1abcee9dbbb51c98deed9b1164f7789a7d943e
Alkivi <admin@alkivi.fr>
2015-01-09 17:01:56 +0100

    fix(service): service smokeping status is broken
    On debian specifically
    so workaround using hasstatus -> false
    and status with pidof (but ugly)

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,8 +3,10 @@ class smokeping::service {
   $ensure = $smokeping::start ? { true => running, false => stopped, default => undef }
 
   service { 'smokeping':
-    ensure   => $ensure,
-    enable   => $smokeping::enable,
+    ensure    => $ensure,
+    enable    => $smokeping::enable,
+    hasstatus => false,
+    status    => 'pidof "/usr/sbin/smokeping [FPing]"',
   }
 
 }
